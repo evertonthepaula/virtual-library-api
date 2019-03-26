deed4a67027354df96bae5f6b22a156d72216ccb
Everton de Paula <you@example.com>
2019-03-26 18:56:02 -0300

    Configurar Pm2 para rodar  ambiente de desenvolvimento e produção

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,11 +12,13 @@ const logger = require("./routes/config/winston");
 requireDir('./models/');
 mongoose.connect(`${process.env.DB_DBMS}://${process.env.DB_HOST}:${process.env.DB_PORT}/nodeapi`, { useNewUrlParser: true });
 
+
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
-app.use('/api/', require('./routes'));  
 app.use(express.urlencoded({extended: true}));
 
-app.listen(3002, () => logger.info(`API started in: ${process.env.NODE_ENV} ,  http://localhost:3002/api`));
+app.use('/api/', require('./routes'));  
+
+app.listen(process.env.APP_PORT, () => logger.info(`${process.env.APP_NAME} started in: http://localhost:${process.env.APP_PORT}, and use mode: "${process.env.NODE_ENV}" |`));
